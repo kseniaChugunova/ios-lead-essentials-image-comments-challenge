8ec4f00aad2c81b1d09cfd37038de8d83d575e50
ksenia <ksenia@bekitzur.com>
2021-09-11 08:37:06 +0300

    test Comments view title is localized

--- EssentialFeed/EssentialFeed/ImageComments/Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/ImageComments/Presentation/ImageCommentsPresenter.swift
@@ -5,6 +5,14 @@
 import Foundation
 
 public final class ImageCommentsPresenter {
+	public static var title: String {
+		NSLocalizedString(
+			"COMMENTS_VIEW_TITLE",
+			tableName: "ImageComments",
+			bundle: Bundle(for: ImageCommentsPresenter.self),
+			comment: "Title for the comments view")
+	}
+
 	public static func map(_ comments: [ImageComment]) -> ImageCommentsViewModel {
 		ImageCommentsViewModel(comments: comments)
 	}
--- EssentialFeed/EssentialFeedTests/Image Comments/Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/Presentation/ImageCommentsPresenterTests.swift
@@ -6,12 +6,28 @@ import XCTest
 @testable import EssentialFeed
 
 class ImageCommentsPresenterTests: XCTestCase {
+	func test_title_isLocalized() {
+		XCTAssertEqual(ImageCommentsPresenter.title, localized("COMMENTS_VIEW_TITLE"))
+	}
+
 	func test_map_createsViewModel() {
 		let comments = uniqueImageComments()
 
 		let viewModel = ImageCommentsPresenter.map(comments)
 
 		XCTAssertEqual(viewModel.comments, comments)
+	}
+
+	// MARK: - Helpers
+
+	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+		let table = "ImageComments"
+		let bundle = Bundle(for: ImageCommentsPresenter.self)
+		let value = bundle.localizedString(forKey: key, value: nil, table: table)
+		if value == key {
+			XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+		}
+		return value
 	}
 }
 
